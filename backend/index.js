a4d2035ff66044cae086c5409673f3a490cb67ad
Antonio Sala Barbosa <sala.antonio2020@gmail.com>
2022-08-26 13:34:00 -0300

    Conexão permitida pelo CORS, falta exibir os itens no Front

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
-// Teste
-
 const Express = require('express');
+const cors = require('cors');
+const path = require('path');
+
 const express = Express();
 
-express.get('/', (req, res, next)=>{
-    res.send('Ok');
+express.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    express.use(cors());
     next();
+});
+
+express.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/modules/test.json'));
 });
 
 express.listen(3000);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,20 @@
+//import {useState} from 'react'
+
 function App() {
+
+  let res = [];
+  async function getData(){
+    const response = await fetch('http://localhost:3000/')
+    .then(r => r.json());
+
+    console.log(response);
+  }
+  getData();
+
   return (
-    <h1>Teste</h1>
+    <>
+      <h1>Teste</h1>
+    </>
   )
 }
 
